3421dd1c42eeba871ea1a6226cf8210e8d0d610f
Kusum Nebhwani <kusumnebhwani4@gmail.com>
2024-08-01 12:50:47 +0530

    added routes for more readibility

--- app.js
+++ app.js
@@ -75,14 +75,7 @@ app.use((req,res,next)=>{
   next();
 
 });
-// app.get('/demoUser',async(req,res)=>{
-//   let fakeUser = new User({
-//     email : "kusum@gmail.com",
-//     username : "kusum@24"
-//   });
-//   let registeredUser = await User.register(fakeUser,"helloworld");
-//   res.send(registeredUser);
-// })
+
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 app.use("/",users);
